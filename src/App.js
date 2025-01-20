38dc5e00edec83b8d2962bbfddf63846fd20a19a
tuandaynek <103641348+tuandaynek@users.noreply.github.com>
2025-01-20 20:28:03 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar/Navbar.jsx";
+import Product from "./components/product/Product.jsx";
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Product />
+    </>
+  );
+}
+
+export default App;
